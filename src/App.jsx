a56b380af9d60c1bc0dcd4c013f6e14d3cc929ac
sourovroy999 <sourovroy3050@gmail.com>
2025-04-07 20:05:03 +0600

    navbar and hero section done

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import './App.css'
+import logo from './assets/logo.png'
+import logoFooter from './assets/logo-footer.png'
+import bgShadow from './assets/bg-shadow.png'
+import bannerMain from './assets/banner-main.png'
+
+
+
+function App() {
+
+  return (
+    <>
+
+      <div className='max-w-6xl mx-auto'>
+
+        <nav className='flex justify-between mt-10'>
+
+
+          <img className='' src={logo} alt="" />
+
+          <div className='flex gap-7 text-xl items-center'>
+            <a href="">Home</a>
+            <a href="">Fixture</a>
+            <a href="">Teams</a>
+            <a href="">Schedule</a>
+            <button className='border rounded-[10px] w-[125px] h-[52px] flex gap-3 justify-center items-center text-center'><p className='bold '>0 Coin</p> <img className='w-7 h-7' src="https://img.icons8.com/?size=100&id=sPBQkuep9vDA&format=png&color=000000" alt="" /></button>
+          </div>
+
+        </nav>
+
+        {/* hero section start */}
+        <div className='text-center space-y-5 mt-6 bg-black bgImage rounded-3xl h-[545px] content-center'>
+          <img className='flex mx-auto' src={bannerMain} alt="" />
+          <h1 className='text-5xl text-white'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+          <p className='text-white'>Beyond Boundaries Beyond Limits</p>
+
+
+          <div className='content-center mx-auto border-[#E7FE29] border  w-[225px] h-16 rounded-3xl'>
+
+            <button className=' w-[200px] h-12 rounded-2xl bg-[#E7FE29] font-bold '>Claim Your Free Credit</button>
+          </div>
+        </div>
+        {/* hero section end */}
+
+        {/* main section start */}
+
+            <div className='flex justify-between mt-7 items-center'>
+              <div>Available Players</div>
+              <div className='w-72 h-14'>
+                <button className='w-1/2 h-full bg-[#E7FE29] rounded-l-xl font-bold'>Available</button>
+                <button className='w-1/2 h-full bg-[#29e2fe] rounded-r-2xl'>Selected (0) </button>
+              </div>
+            </div>
+
+
+        {/* main section end */}
+
+
+      </div >
+    </>
+  )
+}
+
+export default App
